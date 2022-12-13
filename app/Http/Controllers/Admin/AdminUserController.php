9174f146f51269cd2bd2c662efa60d8be49424fd
irham3 <irham3@gmail.com>
2022-12-14 05:41:47 +0700

    fix logo

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -25,7 +25,9 @@ class AdminUserController extends Controller
             return DataTables::of($users)
             ->addColumn('action', function($user) {
                 $html = '
-                <a class="btn btn-info" href="/userView/'.$user->id.'">Edit</a>
+                <a class="btn btn-info" href="{{ url("admin/users/view") }}'.$user->id.'">View</a>
+                <a class="btn btn-warning" href="{{ url("admin/users/edit") }}'.$user->id.'">Edit</a>
+                <a class="btn btn-danger" href="{{ url("admin/users/delete") }}'.$user->id.'">Delete</a>
                 ';
                 return $html;
             })
